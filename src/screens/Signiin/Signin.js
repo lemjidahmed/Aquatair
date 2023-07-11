ad01e200c2ccbe54f6378a6e5fb791eec47f53ed
abc <abc@gmail.om>
2023-07-11 11:53:08 +0200

    first commit

---
+++ src/screens/Signiin/Signin.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import logo from '../../../assets/logo.png';
+import Custominput from '../../components/CustomInput/Custominput';
+const Signin = () => {
+
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
+    const {height} = useWindowDimensions();
+    return(
+        <View style={styles.root}>
+          
+          <Image source={logo} style={[styles.logo, {height: height * 0.3}]} resizeMode="contain"/>
+          <Custominput placeholder={"Username"} value={username} setValue={setUsername} />
+          <Custominput placeholder={"Password"} value={password} setValue={setPassword} secureTextEntry={true}/>
+        </View>
+    );
+    
+};
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+         
+    },
+    logo: {
+        width: 90,
+        height: 90,
+        
+    },
+});
+
+export default Signin;
